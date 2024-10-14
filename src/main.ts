2eac368a22c5671714589ed36fc82ed7584b0a3a
Beepered <bktrieu@gmail.com>
2024-10-14 16:24:26 -0700

    Step 3 (drawing event listener)

--- src/main.ts
+++ src/main.ts
@@ -6,38 +6,51 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
 
-const test = document.createElement("h1")
-test.innerHTML = "fart"
-app.append(test)
+const test = document.createElement("h1");
+test.innerHTML = "fart";
+app.append(test);
 
 const canvas = document.createElement("canvas");
-canvas.id = 'canvas'; // adds element to css class
+canvas.id = "canvas"; // adds element to css class
 canvas.height = 256;
 canvas.width = 256;
-app.append(canvas)
+app.append(canvas);
 
-const drawing = canvas.getContext("2d");
-let isDrawing = false
-let x = 0;
-let y = 0;
-canvas.addEventListener("mousedown", (cursor)=>{
-    x = cursor.offsetX;
-    y = cursor.offsetY;
-    isDrawing = true;
-})
-canvas.addEventListener("mousemove", (cursor) => {
-    if (isDrawing) {
-      drawLine(drawing, x, y, cursor.offsetX, cursor.offsetY);
-      x = cursor.offsetX;
-      y = cursor.offsetY;
+//drawing
+const drawing = canvas.getContext("2d")!;
+interface Point {
+    x: number;
+    y: number;
+}
+type Line = Point[];
+const lines: Line[] = [];
+const cursor = { active: false, x: 0, y: 0 };
+let currentLine: Line = [];
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+    currentLine = [];
+    currentLine.push({ x: e.offsetX, y: e.offsetY });
+    lines.push(currentLine);
+    canvas.dispatchEvent(event)
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+        currentLine.push({ x: e.offsetX, y: e.offsetY });
+        canvas.dispatchEvent(event)
     }
 });
 canvas.addEventListener("mouseup", (e) => {
-    if (isDrawing) {
-      drawLine(drawing, x, y, e.offsetX, e.offsetY);
-      x = 0;
-      y = 0;
-      isDrawing = false;
+    if (cursor.active) {
+        cursor.x = 0;
+        cursor.y = 0;
+        cursor.active = false;
+        currentLine.push({ x: e.offsetX, y: e.offsetY });
+        canvas.dispatchEvent(event)
     }
 });
 function drawLine(drawing, x1, y1, x2, y2) {
@@ -50,11 +63,32 @@ function drawLine(drawing, x1, y1, x2, y2) {
     drawing.closePath();
 }
 
-//clear button
-const clearButton = document.createElement("button")
-clearButton.className = "clear_button"
-clearButton.innerHTML = "CLEAR"
-app.append(clearButton)
-clearButton.addEventListener("mousedown", ()=>{
+function drawAllLines() {
     drawing.clearRect(0, 0, canvas.height, canvas.width);
-})
+    for (const line of lines) {
+        drawing.beginPath();
+        drawing.strokeStyle = "black";
+        drawing.lineWidth = 1;
+        const { x, y } = line[0];
+        drawing.moveTo(x, y);
+        for (const { x, y } of line) {
+            drawing.lineTo(x, y);
+        }
+        drawing.stroke();
+        drawing.closePath();
+    }
+}
+
+//clear button
+const clearButton = document.createElement("button");
+clearButton.className = "clear_button";
+clearButton.innerHTML = "CLEAR";
+app.append(clearButton);
+clearButton.addEventListener("mousedown", () => {
+    drawing.clearRect(0, 0, canvas.height, canvas.width);
+});
+
+const event = new Event("drawing-changed");
+canvas.addEventListener("drawing-changed", () => { // what happens when canvas receives event
+    drawAllLines();
+});
