8f069a6995aae3df63e5c0415e8c1304e85789e1
Beepered <bktrieu@gmail.com>
2024-10-14 13:34:13 -0700

    Step 1 (canvas)

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,13 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const test = document.createElement("h1")
+test.innerHTML = "fart"
+app.append(test)
+
+const canvas = document.createElement("canvas");
+canvas.id = 'canvas'; // adds element to css class
+canvas.height = 256;
+canvas.width = 256;
+app.append(canvas)
