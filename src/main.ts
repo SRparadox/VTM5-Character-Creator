54c47fe1a5457d238467b6f9624e9a8cddc1d969
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-14 10:23:07 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,13 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const appTitle = document.createElement("h1");
+appTitle.textContent = "My Cool App"
+app.append(appTitle);
+
+const canvas = document.createElement("canvas") as HTMLCanvasElement;
+app.append(canvas);
+canvas.width = 256;
+canvas.height = 256;
+
