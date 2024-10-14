ecd296d482bf9f3a28ee1817db7e812c0baef618
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-14 09:52:54 -0700

    Test push

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Jack's Great Game!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
