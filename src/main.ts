26571825c41ed4df984b1a302ffb2f7baef95c5d
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-14 10:51:18 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,37 @@ app.append(canvas);
 canvas.width = 256;
 canvas.height = 256;
 
+const drawing = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+const lines = [];
+const redoLines = [];
+
+const cursor = {active: false, x:0, y:0}; 
+//mouse moving
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+})
+
+canvas.addEventListener("mouseup", (e)=> {
+    cursor.active = false;
+})
+
+canvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+        drawing.beginPath();
+        drawing.moveTo(cursor.x, cursor.y);
+        drawing.lineTo(e.offsetX, e.offsetY);
+        drawing.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+})
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+clearButton.addEventListener("click", () => {
+    drawing.clearRect(0, 0, canvas.width, canvas.height);
+})
