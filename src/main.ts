69cc8f665460c8c1b9677b2541b5a8cb4a45f74f
jackson <jackson.mclane20@gmail.com>
2024-10-15 16:49:16 -0700

    added canvas and header, basic css. step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
 
-const APP_NAME = "Hello There";
+const APP_NAME = "Hello Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+const header  = document.createElement("h1");
+header.innerHTML = "Sketchpad";
+app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
+
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
