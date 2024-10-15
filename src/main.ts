1c94f7f498334c5609a834c4dec89a36d495218c
Andrew Byi <abyi@ucsc.edu>
2024-10-15 04:00:23 +0000

    added canvas  with a shadow and curved edges

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import "./style.css";
 
 const APP_NAME = "Beep";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const Title = "Title";
+
+const header = document.createElement("h1");
+header.innerHTML = Title;
+app.append(header);
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 256, 256);
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
