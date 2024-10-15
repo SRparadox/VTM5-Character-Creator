58820e90e20419570705432dd74554211db5f6bf
Zeke Davidson <zkdavids@ucsc.edu>
2024-10-14 20:36:23 -0700

    Step 1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Painter";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.innerHTML = "Painter";
+
+const CANVAS_HEIGHT = 256;
+const CANVAS_WIDTH = 256;
+
+const canvas = document.createElement("canvas");
+canvas.height = CANVAS_HEIGHT;
+canvas.width = CANVAS_WIDTH;
+
+app.append(title);
+app.append(canvas);
