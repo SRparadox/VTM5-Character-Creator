6240ef208981401dc053cfa3eb8a8db659a79da5
Andrew Byi <abyi@ucsc.edu>
2024-10-15 06:06:42 +0000

    added drawing ability to canvas

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import "./style.css";
 
+let isDraw = true;
+let x = 0;
+let y = 0;
+
 const APP_NAME = "Beep";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canv = document.getElementById("canvas");
+const ctx = canv.getContext("2d");
 
 const Title = "Title";
 
@@ -20,3 +24,24 @@ onmousemove = (draw) => {};
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+//functions borrowed from website: https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+canv.addEventListener("mousemove", (e) => 
+{
+    if (isDraw) {
+        drawLine(ctx, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+  }
+  
