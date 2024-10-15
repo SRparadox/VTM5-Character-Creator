34f663d22582e1951a76e12d7b0f4bd9526ff01f
Zeke Davidson <zkdavids@ucsc.edu>
2024-10-14 21:11:10 -0700

    Step 2

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,36 @@ canvas.width = CANVAS_WIDTH;
 
 app.append(title);
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
