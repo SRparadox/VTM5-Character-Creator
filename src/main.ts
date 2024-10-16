8c93e795377e082d9ac735b41aff38045cf31de7
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-16 16:35:04 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -21,52 +21,65 @@ interface Point {
     x: number,
     y: number,
 }
-const lines: Point[][] = [];
-const redoLines: Point[][] = [];
-let currentLine: Point[] = [];
 
-const cursor = {active: false, x:0, y:0}; 
+interface Displayable {
+    display(ctx: CanvasRenderingContext2D): void
+}
+
+class MarkerCommand {
+    line: Point[];
+    constructor(){
+        this.line = [];
+    }
+    display(ctx: CanvasRenderingContext2D) {
+        if (this.line.length > 0) {
+            ctx.beginPath();
+            const startingPoint: Point = this.line[0];
+            ctx.moveTo(startingPoint.x,startingPoint.y);
+            for (const point of this.line) {
+                ctx.lineTo(point.x, point.y);
+            }
+            ctx.stroke();
+        };
+    }
+    drag(x: number, y: number) {
+        currentCommand.line.push({x, y});
+    }
+}
+
+const displayableCommands: Displayable[] = [];
+let currentCommand = new MarkerCommand();
+const redoCommands: Displayable[] = [];
+
+const cursor = {active: false}; 
 
 //mouse moving
 const drawingChangeEvent = new Event('drawing-changed');
 
 canvas.addEventListener("mousedown", (e) => {
     cursor.active = true;
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
-    currentLine = [];
-    lines.push(currentLine);
-    currentLine.push({ x: cursor.x, y: cursor.y });
+    currentCommand = new MarkerCommand();
+    currentCommand.drag(e.offsetX, e.offsetY);
+    displayableCommands.push(currentCommand);
     canvas.dispatchEvent(drawingChangeEvent);
 })
 
-canvas.addEventListener("mouseup", (e)=> {
+canvas.addEventListener("mouseup", ()=> {
     cursor.active = false;
-    currentLine = [];
     canvas.dispatchEvent(drawingChangeEvent);
 })
 
 canvas.addEventListener("mousemove", (e) => {
     if (cursor.active) {
-        cursor.x = e.offsetX;
-        cursor.y = e.offsetY;
-        currentLine.push({ x: cursor.x, y: cursor.y });
+        currentCommand.line.push({x: e.offsetX, y: e.offsetY});
         canvas.dispatchEvent(drawingChangeEvent);
     }
 })
 
 canvas.addEventListener("drawing-changed", (e) => {
     drawing.clearRect(0, 0, canvas.width, canvas.height);
-    for (const line of lines) {
-        if (lines.length > 0) {
-            drawing.beginPath();
-            const startingPoint: Point = line[0];
-            drawing.moveTo(startingPoint.x,startingPoint.y);
-            for (const point of line) {
-                drawing.lineTo(point.x, point.y);
-            }
-            drawing.stroke();
-        }
+    for (const command of displayableCommands) {
+        command.display(drawing);
     }
 })
 
@@ -75,28 +88,28 @@ clearButton.innerHTML = "clear";
 app.append(clearButton);
 clearButton.addEventListener("click", () => {
     drawing.clearRect(0, 0, canvas.width, canvas.height);
-    lines.length = 0;
+    displayableCommands.length = 0;
 })
 
 const undoButton = document.createElement("button");
 undoButton.innerHTML = "undo";
 app.append(undoButton);
 undoButton.addEventListener("click", () => {
-   lineMover(redoLines, lines);
+   mover(displayableCommands, redoCommands);
 });
 
 const redoButton = document.createElement("button");
 redoButton.innerHTML = "redo";
 app.append(redoButton);
 redoButton.addEventListener("click", () => {
-   lineMover(lines, redoLines); 
+   mover(redoCommands, displayableCommands);
 });
 
-function lineMover (addingLinesTo: Point[][], takingLinesFrom: Point[][]) {
-    if(takingLinesFrom.length != 0) {
-        const movedLine = takingLinesFrom.pop();
-        if (movedLine){
-            addingLinesTo.push(movedLine);
+function mover (source: Displayable[], dest: Displayable[]) {
+    if(source.length != 0) {
+        const movedLine = source.pop();
+        if (movedLine) {
+            dest.push(movedLine);
         }
         canvas.dispatchEvent(drawingChangeEvent);
     }
