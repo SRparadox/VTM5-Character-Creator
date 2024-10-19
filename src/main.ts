aaee1ae1cb7bbf82c369e44f597fe10d9c5aa6a3
Zeke Davidson <zkdavids@ucsc.edu>
2024-10-18 21:41:36 -0700

    Step 4

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,16 @@ canvas.width = CANVAS_WIDTH;
 
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "clear";
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "undo";
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "redo";
 
 app.append(title);
 app.append(canvas);
 app.append(clearButton);
+app.append(undoButton);
+app.append(redoButton);
 
 interface point {
   x: number;
@@ -30,6 +36,7 @@ interface point {
 type line = point[];
 let currentLine: line = [];
 const lines: line[] = [];
+const redoLines: line[] = [];
 
 const ctx = canvas.getContext("2d");
 
@@ -60,11 +67,6 @@ canvas.addEventListener("drawing-changed", () => {
   redraw();
 });
 
-clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  lines.splice(0, lines.length);
-});
-
 function redraw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (const line of lines) {
@@ -79,3 +81,23 @@ function redraw() {
     }
   }
 }
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.splice(0, lines.length);
+  redoLines.splice(0, redoLines.length);
+});
+
+undoButton.addEventListener("click", () => {
+  if (lines.length > 0) {
+    redoLines.push(lines.pop());
+    canvas.dispatchEvent(new Event("drawing-changed"));
+  }
+});
+
+redoButton.addEventListener("click", () => {
+  if (redoLines.length > 0) {
+    lines.push(redoLines.pop());
+    canvas.dispatchEvent(new Event("drawing-changed"));
+  }
+});
