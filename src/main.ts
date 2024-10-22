77e111a69eec94e84be045cf6e9d19a59b066faf
jackson <jackson.mclane20@gmail.com>
2024-10-21 19:00:52 -0700

    added multiple stickers, step 8 complete

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ interface Point {
   y: number;
 }
 
-class LineCommand {
+interface DrawCommand {
+  display(ctx: CanvasRenderingContext2D): void;
+  drag(x: number, y: number): void;
+}
+
+class LineCommand implements DrawCommand {
   points: Point[];
   thickness: number = 1;
 
@@ -29,6 +34,28 @@ class LineCommand {
   }
 }
 
+class StickerCommand implements DrawCommand {
+  x: number;
+  y: number;
+  sticker: string;
+
+  constructor(x: number, y: number, sticker: string) {
+    this.x = x;
+    this.y = y;
+    this.sticker = sticker;
+  }
+
+  drag(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  display(ctx: CanvasRenderingContext2D) {
+    ctx.font = "32px monospace";
+    ctx.fillText(this.sticker, this.x - 16, this.y + 16);
+  }
+}
+
 class CursorCommand {
   x: number;
   y: number;
@@ -37,12 +64,12 @@ class CursorCommand {
   constructor(x: number, y: number, active: boolean) {
     this.x = x;
     this.y = y;
-    this.active = active
+    this.active = active;
   }
 
   display(ctx: CanvasRenderingContext2D) {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, Math.max(lineThickness/2,1), 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, Math.max(lineThickness / 2, 1), 0, Math.PI * 2);
     ctx.fill();
   }
 }
@@ -85,47 +112,82 @@ const thickMarkerButton = document.createElement("button");
 thickMarkerButton.innerHTML = "Thick";
 app.append(thickMarkerButton);
 
+const stickers = [`🗿`, `❤️`, `👹`];
+const DRAW_MODE_LINE = 12;
+const DRAW_MODE_STICKER = 13;
+
+for (const sticker of stickers) {
+  const stickerButton = document.createElement("button");
+  stickerButton.innerHTML = sticker;
+  stickerButton.addEventListener("click", (_event) => {
+    canvas.dispatchEvent(new CustomEvent("tool-moved"));
+    canvas.dispatchEvent(new CustomEvent("sticker-mode", { detail: sticker }));
+  });
+  app.append(stickerButton);
+}
+
+let stickerCursor: StickerCommand | null = null;
+let currentSticker = stickers[0];
+
+canvas.addEventListener("sticker-mode", ((event: CustomEvent) => {
+  drawMode = DRAW_MODE_STICKER
+  currentSticker = event.detail;
+  stickerCursor = new StickerCommand(0, 0, currentSticker);
+}) as EventListener);
+
 const ctx = canvas.getContext("2d")!;
 
 let cursor: CursorCommand | null = null;
 
 canvas.addEventListener("mouseout", (_event) => {
   cursor = null;
+  stickerCursor = null;
   canvas.dispatchEvent(new CustomEvent("tool-moved"));
 });
 
-canvas.addEventListener("mouseenter", (_event) => {
-  cursor = new CursorCommand(_event.offsetX, _event.offsetY, false);
+canvas.addEventListener("mouseenter", (event) => {
+  cursor = new CursorCommand(event.x, event.y, false);
+  stickerCursor = new StickerCommand(event.x, event.y, currentSticker);
   canvas.dispatchEvent(new CustomEvent("tool-moved"));
 });
 
-const lines: LineCommand[] = [];
+const drawings: DrawCommand[] = [];
 
-let currentLine: LineCommand = new LineCommand([], 1);
+let drawMode = DRAW_MODE_LINE
+let currentDrawing: DrawCommand = new LineCommand([], 1);
 
 canvas.addEventListener("mousedown", (_event) => {
-  if (cursor) {
-    currentLine = new LineCommand([{ x: cursor.x, y: cursor.y }], lineThickness);
-    currentLine.drag(cursor.x, cursor.y);
-    lines.push(currentLine);
+  if(cursor){
+    if(drawMode == DRAW_MODE_LINE){
+      currentDrawing = new LineCommand([{ x: cursor.x, y: cursor.y }], lineThickness);
+    } else if(drawMode == DRAW_MODE_STICKER){
+      currentDrawing = new StickerCommand(cursor.x, cursor.y, currentSticker);
+    }
+    currentDrawing.drag(cursor.x, cursor.y);
+    drawings.push(currentDrawing);
     cursor.active = true;
   }
   canvas.dispatchEvent(new CustomEvent("drawing-changed"));
 });
 
 canvas.addEventListener("mousemove", (_event) => {
-  if (cursor) {
+  if(cursor){
     cursor.x = _event.offsetX;
     cursor.y = _event.offsetY;
-
-    currentLine.drag(cursor.x, cursor.y);
-    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+    currentDrawing.drag(cursor.x, cursor.y);
   }
+  
+  canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+  canvas.dispatchEvent(new CustomEvent("tool-moved"));
 });
 
 canvas.addEventListener("mouseup", (_event) => {
-  currentLine = new LineCommand([], lineThickness);
   if(cursor){
+    if(drawMode == DRAW_MODE_LINE){
+      currentDrawing = new LineCommand([], lineThickness);
+    } else if(drawMode == DRAW_MODE_STICKER){
+      currentDrawing = new StickerCommand(cursor.x, cursor.y, (currentDrawing as StickerCommand).sticker);
+    }
     cursor.active = false;
   }
   canvas.dispatchEvent(new CustomEvent("drawing-changed"));
@@ -133,11 +195,14 @@ canvas.addEventListener("mouseup", (_event) => {
 
 function redraw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (const line of lines) {
-    line.display(ctx);
+  for (const drawing of drawings) {
+    drawing.display(ctx);
   }
   if (cursor?.active == false) {
     cursor.display(ctx);
+  }
+  if(drawMode == DRAW_MODE_STICKER && stickerCursor){
+    stickerCursor.display(ctx);
   }
 }
 
@@ -146,27 +211,29 @@ canvas.addEventListener("drawing-changed", (_event) => {
 });
 
 canvas.addEventListener("tool-moved", (_event) => {
+  if(cursor && stickerCursor){
+    stickerCursor.drag(cursor.x, cursor.y);
+  }
   redraw();
 });
 
 clearButton.addEventListener("click", () => {
-  lines.length = 0;
+  drawings.length = 0;
   canvas.dispatchEvent(new CustomEvent("drawing-changed"));
 });
 
-
-const redoStack: LineCommand[] = [];
+const redoStack: DrawCommand[] = [];
 
 redoButton.addEventListener("click", () => {
   const line = redoStack.pop();
   if (line) {
-    lines.push(line);
+    drawings.push(line);
     canvas.dispatchEvent(new CustomEvent("drawing-changed"));
   }
 });
 
 undoButton.addEventListener("click", () => {
-  const line = lines.pop();
+  const line = drawings.pop();
   if (line) {
     redoStack.push(line);
     canvas.dispatchEvent(new CustomEvent("drawing-changed"));
@@ -174,17 +241,17 @@ undoButton.addEventListener("click", () => {
 });
 
 let lineThickness = 1;
-
 thinMarkerButton.addEventListener("click", () => {
-  lineThickness = 1;
-  canvas.dispatchEvent(new CustomEvent("marker-changed"));
+  //detail is the thickness of the marker
+  canvas.dispatchEvent(new CustomEvent("marker-changed", { detail: 1 }));
 });
 
 thickMarkerButton.addEventListener("click", () => {
-  lineThickness = 5;
-  canvas.dispatchEvent(new CustomEvent("marker-changed"));
+  //detail is the thickness of the marker
+  canvas.dispatchEvent(new CustomEvent("marker-changed", { detail: 5 }));
 });
 
-canvas.addEventListener("marker-changed", (_event) => {
-  currentLine.thickness = lineThickness;
-});
+canvas.addEventListener("marker-changed", ((event: CustomEvent) => {
+  drawMode = DRAW_MODE_LINE
+  lineThickness = event.detail;
+}) as EventListener);
