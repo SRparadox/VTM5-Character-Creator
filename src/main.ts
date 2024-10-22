f25841a7d9a4191d8ec0e5704bc3d9e161e75821
Izaiah <iilozano@ucsc.edu>
2024-10-22 10:07:00 -0700

    Step 1 Complete:  Added app title + Canvas w/ dropshadow

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Draw your hearts content";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+
+function app_setup() {
+
+    //App Title Settings
+    const title = document.createElement('h1');
+    title.textContent = 'Colors of the Wind';
+    document.body.appendChild(title);
+
+    //Canvas Settings
+    const canvas = document.createElement('canvas');
+    canvas.width = 256;
+    canvas.height = 256;
+    canvas.id = 'appCanvas';
+    document.body.appendChild(canvas);
+
+}
+
+document.addEventListener('DOMContentLoaded', app_setup);
