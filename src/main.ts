eba0e7d434707bb5a2ed72d43f14e69ede82c153
jible <jamezman88th@gmail.com>
2024-11-15 16:23:37 -0800

    Code deoderizing complete!
    All non-button HTML Elements are now created in the HMTL file.
    
    All button HTML elements are now created using create button function.
    The button info is now consoldiated so the "on click" callback function is also definied in the same space as the buttons.
    
    All global variables were moved to the top, after the class definitions.
    
    Lastly, each section is labeled.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import "./style.css";
 
+//--------------------------------------------------------------------------
+//  Classes and Interfaces
+//--------------------------------------------------------------------------
+
 interface Point {
   x: number;
   y: number;
@@ -96,88 +100,149 @@ class CursorCommand {
     ctx.fill();
   }
 }
-
+//------------------------------------------------------------------------------
+// Consts and globals
+//------------------------------------------------------------------------------
+const redoStack: DrawCommand[] = [];
 const APP_NAME = "Sketchpad Demo";
-document.title = APP_NAME;
-const app = document.querySelector<HTMLDivElement>("#app")!;
-
-const header = document.createElement("h1");
-header.innerHTML = "Sketchpad";
-app.append(header);
-
-const canvas = document.createElement("canvas");
-canvas.width = 256;
-canvas.height = 256;
-canvas.style.cursor = "none";
-app.append(canvas);
-
-app.append(document.createElement("br"));
-
-const clearButton = document.createElement("button");
-clearButton.innerHTML = "clear";
-app.append(clearButton);
-
-const undoButton = document.createElement("button");
-undoButton.innerHTML = "undo";
-app.append(undoButton);
-
-const redoButton = document.createElement("button");
-redoButton.innerHTML = "redo";
-app.append(redoButton);
-
-const exportButton = document.createElement("button");
-exportButton.innerHTML = "Export";
-app.append(exportButton);
-
-app.append(document.createElement("br"));
-
 const colors = ["black", "red", "green", "blue", "purple"];
 
-for(const color of colors){
-  const colorButton = document.createElement("button");
-  colorButton.innerHTML = color;
-  colorButton.addEventListener("click", () => {
-    if(drawMode == DRAW_MODE_LINE){
-      lineColor = color;
-    }
-  });
-  app.append(colorButton);
-}
-
-app.append(document.createElement("br"));
-
-const thinMarkerButton = document.createElement("button");
-thinMarkerButton.innerHTML = "Thin";
-app.append(thinMarkerButton);
-
-const thickMarkerButton = document.createElement("button");
-thickMarkerButton.innerHTML = "Thick";
-app.append(thickMarkerButton);
+const drawings: DrawCommand[] = [];
+const THIN_LINE_PX = 2;
+const THICK_LINE_PX = 8;
+let lineThickness = THIN_LINE_PX;
 
 const stickers = [`🗿`, `❤️`, `👹`];
 const STICKER_SIZE = 16;
 const DRAW_MODE_LINE = 12;
 const DRAW_MODE_STICKER = 13;
 
-for (const sticker of stickers) {
-  const stickerButton = document.createElement("button");
-  stickerButton.innerHTML = sticker;
-  stickerButton.addEventListener("click", (_event) => {
-    canvas.dispatchEvent(new CustomEvent("tool-moved"));
-    canvas.dispatchEvent(new CustomEvent("sticker-mode", { detail: sticker }));
-  });
-  app.append(stickerButton);
+
+let cursor: CursorCommand | null = null;
+let drawMode = DRAW_MODE_LINE
+let currentDrawing: DrawCommand = new LineCommand([], 1);
+let lineColor = "black";
+
+//------------------------------------------------------------------------------
+// Adding/Configuring HTML Elements
+//------------------------------------------------------------------------------
+document.title = APP_NAME;
+const app = document.querySelector<HTMLDivElement>("#app")!;
+const header = document.querySelector<HTMLCanvasElement>("#h1");
+const canvas = document.querySelector<HTMLCanvasElement>("#drawingCanvas");
+if (!canvas){
+  throw new Error("Canvas element not found");
+}
+const ctx = canvas.getContext("2d")!;
+
+// Buttons
+function makeButton(innerText, parent, callback){
+  const button = document.createElement("button");
+  button.innerHTML = innerText;
+  parent.append(button);
+  button.addEventListener("click", callback);
+  return(button);
 }
 
-const customStickerButton = document.createElement("button");
-customStickerButton.innerHTML = "Create Sticker";
-app.append(customStickerButton);
+const clearButton = makeButton("clear", app, ()=>{
+  drawings.length = 0;
+  canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+})
+
+const undoButton = makeButton("undo", app, ()=>{
+  const line = drawings.pop();
+  if (line) {
+    redoStack.push(line);
+    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+  }
+})
+
+const redoButton = makeButton("redo", app, ()=>{
+  const line = redoStack.pop();
+  if (line) {
+    drawings.push(line);
+    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+  }
+})
+
+const exportButton = makeButton("Export", app, ()=>{
+  const highDef = document.createElement("canvas");
+  highDef.width = 1024;
+  highDef.height = 1024;
+  const hdctx = highDef.getContext("2d")!;
+  hdctx.scale(4, 4);
+  for (const drawing of drawings) {
+    drawing.display(hdctx);
+  }
+  const anchor = document.createElement("a");
+  anchor.href = highDef.toDataURL("image/png");
+  anchor.download = "sketchpad.png";
+  anchor.click();
+  // Save the drawings to localStorage
+  localStorage.setItem("drawings", JSON.stringify(drawings));
+})
 
 app.append(document.createElement("br"));
 
-const ctx = canvas.getContext("2d")!;
 
-let cursor: CursorCommand | null = null;
+for(const color of colors){
+  makeButton(color, app, ()=>{
+    if(drawMode == DRAW_MODE_LINE){
+      lineColor = color;
+    }
+  })
+}
+
+app.append(document.createElement("br"));
+
+const thinMarkerButton = makeButton("Thin",app, ()=>{
+  canvas.dispatchEvent(new CustomEvent("marker-changed", { detail: THIN_LINE_PX }));
+})
+
+const thickMarkerButton = makeButton("Thick", app, ()=>{
+  canvas.dispatchEvent(new CustomEvent("marker-changed", { detail: THICK_LINE_PX }));
+})
+
+for (const sticker of stickers) {
+  const stickerButton = makeButton("sticker", app, ()=>{
+    canvas.dispatchEvent(new CustomEvent("tool-moved"));
+    canvas.dispatchEvent(new CustomEvent("sticker-mode", { detail: sticker }));
+  });
+}
+
+const customStickerButton = makeButton("Create Sticker", app, ()=> {
+  const sticker = prompt("Enter a sticker");
+  if(sticker){
+    stickers.push(sticker);
+    const stickerButton = makeButton(sticker, app, ()=>{
+      canvas.dispatchEvent(new CustomEvent("tool-moved"));
+      canvas.dispatchEvent(new CustomEvent("sticker-mode", { detail: sticker }));
+    })
+  }
+})
+
+
+app.append(document.createElement("br"));
+
+const loadButton = makeButton("Load", app, ()=>{
+  const storedDrawings = localStorage.getItem("drawings");
+  if (storedDrawings) {
+    const parsedDrawings = JSON.parse(storedDrawings);
+    drawings.length = 0; // Clear the current drawings
+    parsedDrawings.forEach((drawing: any) => {
+      if (drawing.type === 'line') {
+        drawings.push(new LineCommand(drawing.points, drawing.thickness, drawing.color));
+      } else if (drawing.type === 'sticker') {
+        drawings.push(new StickerCommand(drawing.x, drawing.y, drawing.sticker));
+      }
+    });
+    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+  }
+});
+
+// ----------------------------------------------------------------------------------------
+// Canvas listeners
+// ----------------------------------------------------------------------------------------
 
 canvas.addEventListener("mouseout", (_event) => {
   cursor = null;
@@ -191,13 +256,7 @@ canvas.addEventListener("mouseenter", (event) => {
   canvas.dispatchEvent(new CustomEvent("tool-moved"));
 });
 
-const drawings: DrawCommand[] = [];
-const THIN_LINE_PX = 2;
-const THICK_LINE_PX = 8;
 
-let drawMode = DRAW_MODE_LINE
-let currentDrawing: DrawCommand = new LineCommand([], 1);
-let lineColor = "black";
 
 canvas.addEventListener("mousedown", (_event) => {
   if(cursor){
@@ -237,6 +296,9 @@ canvas.addEventListener("mouseup", (_event) => {
 });
 
 function redraw() {
+  if (!canvas){
+    return
+  }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (const drawing of drawings) {
     drawing.display(ctx);
@@ -252,6 +314,10 @@ function redraw() {
   }
 }
 
+// ------------------------------------------------------------------------
+// Canvas signal Listeners
+// ------------------------------------------------------------------------
+
 canvas.addEventListener("drawing-changed", (_event) => {
   redraw();
 });
@@ -261,38 +327,6 @@ canvas.addEventListener("tool-moved", (_event) => {
     stickerCursor.drag(cursor.x, cursor.y);
   }
   redraw();
-});
-
-clearButton.addEventListener("click", () => {
-  drawings.length = 0;
-  canvas.dispatchEvent(new CustomEvent("drawing-changed"));
-});
-
-const redoStack: DrawCommand[] = [];
-
-redoButton.addEventListener("click", () => {
-  const line = redoStack.pop();
-  if (line) {
-    drawings.push(line);
-    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
-  }
-});
-
-undoButton.addEventListener("click", () => {
-  const line = drawings.pop();
-  if (line) {
-    redoStack.push(line);
-    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
-  }
-});
-
-let lineThickness = THIN_LINE_PX;
-thinMarkerButton.addEventListener("click", () => {
-  canvas.dispatchEvent(new CustomEvent("marker-changed", { detail: THIN_LINE_PX }));
-});
-
-thickMarkerButton.addEventListener("click", () => {
-  canvas.dispatchEvent(new CustomEvent("marker-changed", { detail: THICK_LINE_PX }));
 });
 
 canvas.addEventListener("marker-changed", ((event: CustomEvent) => {
@@ -309,58 +343,3 @@ canvas.addEventListener("sticker-mode", ((event: CustomEvent) => {
   currentSticker = event.detail;
   stickerCursor = new StickerCommand(0, 0, currentSticker);
 }) as EventListener);
-
-customStickerButton.addEventListener("click", () => {
-  const sticker = prompt("Enter a sticker");
-  if(sticker){
-    stickers.push(sticker);
-    const stickerButton = document.createElement("button");
-    stickerButton.innerHTML = sticker;
-    stickerButton.addEventListener("click", (_event) => {
-      canvas.dispatchEvent(new CustomEvent("tool-moved"));
-      canvas.dispatchEvent(new CustomEvent("sticker-mode", { detail: sticker }));
-    });
-    app.append(stickerButton);
-  }
-});
-
-exportButton.addEventListener("click", () => {
-  const highDef = document.createElement("canvas");
-  highDef.width = 1024;
-  highDef.height = 1024;
-  const hdctx = highDef.getContext("2d")!;
-  hdctx.scale(4, 4);
-  for (const drawing of drawings) {
-    drawing.display(hdctx);
-  }
-  const anchor = document.createElement("a");
-  anchor.href = highDef.toDataURL("image/png");
-  anchor.download = "sketchpad.png";
-  anchor.click();
-
-  // Save the drawings to localStorage
-  localStorage.setItem("drawings", JSON.stringify(drawings));
-});
-
-// load button
-const loadButton = document.createElement("button");
-loadButton.innerHTML = "Load";
-app.append(loadButton);
-
-loadButton.addEventListener("click", () => {
-  const storedDrawings = localStorage.getItem("drawings");
-  if (storedDrawings) {
-    const parsedDrawings = JSON.parse(storedDrawings);
-    drawings.length = 0; // Clear the current drawings
-    parsedDrawings.forEach((drawing: any) => {
-      if (drawing.type === 'line') {
-        drawings.push(new LineCommand(drawing.points, drawing.thickness, drawing.color));
-      } else if (drawing.type === 'sticker') {
-        drawings.push(new StickerCommand(drawing.x, drawing.y, drawing.sticker));
-      }
-    });
-    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
-  }
-});
-
-
